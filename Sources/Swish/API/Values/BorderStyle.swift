14e578a2ae1844e55cbc1ca71fd814c7616382e8
Mike Green <michael.green@thrive.app>
2020-02-17 09:20:25 +0000

    BorderStyle added

---
+++ Sources/Swish/API/Values/BorderStyle.swift
@@ -0,0 +1,22 @@
+
+/**
+*  Swish
+*  Copyright (c) Michael Green 2020
+*  MIT license, see LICENSE file for details
+*/ 
+
+enum BorderStyle: String {
+
+  case none = "none"
+  case hidden = "hidden"
+  case dotted = "dotted"
+  case dashed = "dashed"
+  case solid = "solid"
+  case double = "double"
+  case groove = "groove"
+  case ridge = "ridge"
+  case inset = "inset"
+  case outset = "outset"
+  case initial = "initial"
+  case inherit = "inherit"
+}
